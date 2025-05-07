360627a8b54a590ca5061ea208e50599b3136796
loueysioua <louey.sioua@insat.ucar.tn>
2025-05-07 21:37:13 +0100

    fix: dependencies

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -7,6 +7,21 @@ import path from "path";
 loadEnvConfig(path.resolve(__dirname, ".."));
 
 const nextConfig: NextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.ts",
+      },
+    },
+  },
   env: {
     UI_BASE_URL: process.env.UI_BASE_URL,
     API_BASE_URL: process.env.API_BASE_URL,
