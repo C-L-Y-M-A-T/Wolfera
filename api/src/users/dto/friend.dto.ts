908903399d328c8916e43e730eae1fc95a8e381a
chadha-grami <chadhagrami1@gmail.com>
2025-05-30 17:16:29 +0100

    feat: refactor user and friend DTOs to extend BaseUserDto

--- api/src/users/dto/friend.dto.ts
+++ api/src/users/dto/friend.dto.ts
@@ -1,55 +1,5 @@
-import { Field, ObjectType } from '@nestjs/graphql';
-import {
-  IsEmail,
-  IsEnum,
-  IsNumber,
-  IsOptional,
-  IsString,
-  IsUUID,
-  Min,
-} from 'class-validator';
-import { Badge } from '../entities/user.entity';
+import { ObjectType } from '@nestjs/graphql';
+import { BaseUserDto } from './base-user.dto';
 
 @ObjectType()
-export class FriendDto {
-  @Field()
-  @IsUUID()
-  id: string;
-
-  @Field()
-  @IsEmail()
-  email: string;
-
-  @Field()
-  @IsString()
-  username: string;
-
-  @Field({ nullable: true })
-  @IsString()
-  @IsOptional()
-  avatar_url?: string;
-
-  @Field(() => [String])
-  @IsEnum(Badge, { each: true })
-  badges: Badge[];
-
-  @Field()
-  @IsNumber()
-  @Min(0)
-  gamesPlayed: number;
-
-  @Field()
-  @IsNumber()
-  @Min(0)
-  gamesWon: number;
-
-  @Field()
-  @IsNumber()
-  @Min(0)
-  gamesAsWerewolf: number;
-
-  @Field()
-  @IsNumber()
-  @Min(0)
-  gamesAsVillager: number;
-}
+export class FriendDto extends BaseUserDto {}
--- api/src/users/dto/user.dto.ts
+++ api/src/users/dto/user.dto.ts
@@ -1,64 +1,14 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { Type } from 'class-transformer';
-import {
-  IsEmail,
-  IsEnum,
-  IsNumber,
-  IsOptional,
-  IsString,
-  IsUUID,
-  Min,
-  ValidateNested,
-} from 'class-validator';
-import { Badge } from '../entities/user.entity';
+import { IsOptional, ValidateNested } from 'class-validator';
+import { BaseUserDto } from './base-user.dto';
 import { FriendDto } from './friend.dto';
 
 @ObjectType()
-export class UserDto {
-  @Field()
-  @IsUUID()
-  id: string;
-
-  @Field()
-  @IsEmail()
-  email: string;
-
-  @Field()
-  @IsString()
-  username: string;
-
-  @Field({ nullable: true })
-  @IsString()
-  @IsOptional()
-  avatar_url?: string;
-
+export class UserDto extends BaseUserDto {
   @Field(() => [FriendDto], { nullable: true })
   @ValidateNested({ each: true })
   @Type(() => FriendDto)
   @IsOptional()
   friends?: FriendDto[];
-
-  @Field(() => [Badge])
-  @IsEnum(Badge, { each: true })
-  badges: Badge[];
-
-  @Field()
-  @IsNumber()
-  @Min(0)
-  gamesPlayed: number;
-
-  @Field()
-  @IsNumber()
-  @Min(0)
-  gamesWon: number;
-
-  @Field()
-  @IsNumber()
-  @Min(0)
-  gamesAsWerewolf: number;
-
-  @Field()
-  @IsNumber()
-  @Min(0)
-  gamesAsVillager: number;
 }
