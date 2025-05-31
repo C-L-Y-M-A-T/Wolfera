c1e48954e0457fb35225753da35c8f0922116131
loueysioua <louey.sioua@insat.ucar.tn>
2025-05-31 17:01:03 +0100

    fix: tailwind config

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "*.{js,ts,jsx,tsx,mdx}",
+    "./src/lib/theme/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
