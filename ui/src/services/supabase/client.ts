fd002c5134b59015cc918eeca82217ceb5ef78bf
Yassine Sallemi <yassine.sallemi.251@gmail.com>
2025-06-02 05:59:38 +0100

    refactor: update authentication logic

--- ui/src/services/supabase/client.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from "@supabase/ssr";
-
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
-
-export const getSupabaseFrontendClient = () => {
-  return createBrowserClient(supabaseUrl, supabaseAnonKey);
-};
