b4e80f424abb322f29037f0360db555d0cb96d0e
chadha-grami <chadhagrami1@gmail.com>
2025-06-03 15:31:04 +0100

    feat: GraphQL decorators in  game and player result entities

--- api/src/game/entities/game.entity.ts
+++ api/src/game/entities/game.entity.ts
@@ -1,3 +1,4 @@
+import { Field, ObjectType, registerEnumType } from '@nestjs/graphql';
 import { BaseEntity } from 'src/utils/generic/base.entity';
 import { Column, Entity, OneToMany, PrimaryColumn } from 'typeorm';
 import { PlayerGameResult } from './player-game-result.entity';
@@ -9,25 +10,42 @@ export enum GameResult {
   ABANDONED = 'abandoned',
 }
 
+registerEnumType(GameResult, {
+  name: 'GameResult',
+});
+
+@ObjectType({ description: 'Represents a game session' })
 @Entity('games')
 export class GameEntity extends BaseEntity {
+  @Field()
   @PrimaryColumn()
   id: string = '';
 
+  @Field(() => Date, {
+    nullable: true,
+  })
   @Column({ type: 'timestamp', nullable: true })
   endedAt: Date | null;
 
+  @Field(() => GameResult, {
+    nullable: true,
+  })
   @Column({ type: 'enum', enum: GameResult, nullable: true })
   result: GameResult | null;
 
+  @Field(() => [PlayerGameResult])
   @OneToMany(() => PlayerGameResult, (playerResult) => playerResult.game, {
     cascade: true,
   })
   playerResults: PlayerGameResult[];
 
+  @Field()
   @Column({ default: false })
   wasCompleted: boolean;
 
+  @Field({
+    nullable: true,
+  })
   @Column({ type: 'varchar', nullable: true })
   winningTeam: string | null;
 }
--- api/src/game/entities/player-game-result.entity.ts
+++ api/src/game/entities/player-game-result.entity.ts
@@ -1,25 +1,38 @@
+import { Field, ObjectType } from '@nestjs/graphql';
 import { RoleName } from 'src/roles';
+import { BaseEntity } from 'src/utils/generic/base.entity';
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
 import { GameEntity } from './game.entity';
 
+@ObjectType()
 @Entity('player_game_result')
-export class PlayerGameResult {
+export class PlayerGameResult extends BaseEntity {
+  @Field()
   @PrimaryColumn()
   playerId: string;
 
+  @Field()
   @PrimaryColumn()
   gameId: string;
 
+  @Field(() => GameEntity, {
+    nullable: true,
+  })
   @ManyToOne(() => GameEntity, (game) => game.playerResults)
   @JoinColumn({ name: 'gameId' })
   game: GameEntity;
 
+  @Field(() => String)
   @Column({ type: 'varchar' })
   role: RoleName;
 
+  @Field()
   @Column()
   isWinner: boolean;
 
+  @Field({
+    defaultValue: false,
+  })
   @Column({ default: false })
   survived: boolean;
 }
